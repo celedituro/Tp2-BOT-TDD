874ecac79563925f189388ac14c587433f4dc83f
vickyylopezz <vlopez@fi.uba.ar>
2022-11-06 09:42:57 -0300

    feat: agregamos steps para escenarios de registracion de usuario

---
+++ features/step_definitions/registracion_usuario_steps.rb
@@ -0,0 +1,27 @@
+When(/^que ingreso "([^"]*)"$/) do |_arg|
+  pending
+end
+
+When(/^me registro$/) do
+  pending
+end
+
+When(/^se muestra el mensaje "([^"]*)"$/) do |_arg|
+  pending
+end
+
+When(/^intento registrarme$/) do
+  pending
+end
+
+When(/^la cuenta no se crea$/) do
+  pending
+end
+
+When(/^que existe un usuario con teléfono "5123\-1234"$/) do |_arg|
+  pending
+end
+
+When(/^ingreso "([^"]*)"$/) do |_arg|
+  pending
+end
